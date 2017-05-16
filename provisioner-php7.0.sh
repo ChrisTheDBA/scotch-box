fb6c03fb33c468bfbfda7c045ba178d20ea682fe
Chris Mathews <christhedba@gmail.com>
2017-05-16 07:10:29 -0400

    added rgdal required libraries

--- provisioner-php7.0.sh
+++ provisioner-php7.0.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
++#!/usr/bin/env bash
 
 # Update PHP Repo
 sudo rm /etc/apt/sources.list.d/ondrej-php5-5_6-trusty.list
@@ -13,3 +13,7 @@ sudo apt-get -y autoremove
 sudo apt-get -y install php7.0 php7.0-common php7.0-cgi php7.0-fpm php7.0-pgsql php7.0-cli php7.0-json php7.0-curl php7.0-mcrypt php7.0-gd php7.0-intl php7.0-mysql php7.0-readline php7.0-mbstring php7.0-sqlite3 php7.0-zip php7.0-xml php7.0-opcache php-memcached php-mongodb php-memcache php-imagick php-xdebug php-redis
 sudo apt-get -y install libapache2-mod-php7.0
 sudo service apache2 restart
+
+#prerequisates for rgdal and rgeos libraries
+sudo aptitude -y install libgdal-dev
+sudo aptitude -y install libproj-de
